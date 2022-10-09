7327e00503574488c6f429edad5046b78ddfb03b
Sakshi Nayan <sakshi12princi@gmail.com>
2022-10-09 19:44:27 +0530

    frontend started

--- Backend/Routes/register.js
+++ Backend/Routes/register.js
@@ -47,7 +47,7 @@ router.get("/detailUser",(req,res)=>{
     try {
         const user = jwt.verify(req.headers.authorization, process.env.SECRET_KEY);
         UserModal.find({userName : user}).then((data)=>{
-          console.log(data)
+          //console.log(data)
           res.status(200).send({user: data});
         }).catch((err)=>{
           res.status(400).send(err);
